4fe74d638a49aa132688f02eb5205a2dd4611cd2
hsldymq <hsldymq@qq.com>
2020-07-02 20:19:09 +0800

    增加启动http server快捷方法

---
+++ http.go
@@ -0,0 +1,67 @@
+package uni_links
+
+import (
+	"net/http"
+	"strings"
+)
+
+type Domain string
+
+type HTTPServer struct {
+	addr          string
+	associations  map[Domain]string
+	afterResponse func(*http.Request)
+}
+
+func NewHTTPServer(addr string, afterResponse func(r *http.Request)) *HTTPServer {
+	return &HTTPServer{
+		addr:          addr,
+		associations:  make(map[Domain]string),
+		afterResponse: afterResponse,
+	}
+}
+
+func (s *HTTPServer) AddAssociation(d Domain, a AssociationMarshaler) error {
+	v, err := a.Marshal()
+	if err != nil {
+		return err
+	}
+
+	s.associations[d] = v
+	return nil
+}
+
+func (s *HTTPServer) Serve() error {
+	return http.ListenAndServe(s.addr, s)
+}
+
+func (s *HTTPServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	host := strings.Split(req.Host, ":")[0]
+	association, ok := s.associations[Domain(host)]
+
+	if req.URL.Path != "/.well-known/apple-app-site-association" {
+		resp.WriteHeader(http.StatusForbidden)
+		_, _ = resp.Write([]byte("invalid path"))
+		goto end
+	}
+
+	if req.Method != http.MethodGet {
+		resp.WriteHeader(http.StatusForbidden)
+		_, _ = resp.Write([]byte("invalid method"))
+		goto end
+	}
+
+	if !ok {
+		resp.WriteHeader(http.StatusNotFound)
+		_, _ = resp.Write([]byte("unsupported domain"))
+	} else {
+		resp.WriteHeader(http.StatusOK)
+		resp.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		_, _ = resp.Write([]byte(association))
+	}
+
+end:
+	if s.afterResponse != nil {
+		s.afterResponse(req)
+	}
+}
